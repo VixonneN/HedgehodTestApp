1c710872c59cd5a7f48699ddfe45b35c440a1f27
sergey <sergey021198@yandex.ru>
2022-01-13 17:03:01 +0300

    Добавил exceptionHandler

--- app/src/main/java/com/example/hedgehodtestapp/presentation/fragments/jokes/JokesFragmentViewModel.kt
+++ app/src/main/java/com/example/hedgehodtestapp/presentation/fragments/jokes/JokesFragmentViewModel.kt
@@ -1,26 +1,33 @@
 package com.example.hedgehodtestapp.presentation.fragments.jokes
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.hedgehodtestapp.data.data_source.Root
-import com.example.hedgehodtestapp.data.network.NetworkModule
 import com.example.hedgehodtestapp.domain.entity.RootEntity
 import com.example.hedgehodtestapp.domain.use_cases.GetJokesUseCase
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.HttpException
-import retrofit2.Response
-import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 class JokesFragmentViewModel(
     private val getJokesUseCase: GetJokesUseCase
 ) : ViewModel() {
+
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        when (throwable) {
+            is UnknownHostException -> _exception.value = "Не найден хост"
+            is SocketTimeoutException -> _exception.value = "Время ожидания вышло"
+            is HttpException -> {
+                when (throwable.code()) {
+                    403 -> _exception.value = "Доступ запрещён"
+                    404 -> _exception.value = "Ничего не найдено"
+                }
+            }
+        }
+    }
 
     private val _jokes = MutableLiveData<RootEntity>()
     val jokes: LiveData<RootEntity> = _jokes
@@ -29,18 +36,12 @@ class JokesFragmentViewModel(
     val exception: LiveData<String> = _exception
 
     fun jokesRequest(number: String) {
-        if (number == "" || number.isEmpty()){
+        if (number == "" || number.isEmpty()) {
             _exception.value = "Введите верное значение"
         } else {
-            viewModelScope.launch {
-                try {
-                    val response = getJokesUseCase.invoke(number.toInt())
-                    _jokes.value = response
-                } catch (e: IOException) {
-                    Log.d(TAG, "jokesRequest: $e")
-                } catch (e: HttpException) {
-                    Log.d(TAG, "jokesRequest: $e")
-                }
+            viewModelScope.launch(handler) {
+                val response = getJokesUseCase.invoke(number.toInt())
+                _jokes.value = response
             }
         }
     }
